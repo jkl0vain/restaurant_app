db87b346ccb4bd5960e8b4497328c84d4de1e1b0
SitiFatimah0403 <223846@student.upm.edu.my>
2025-05-07 00:04:38 +0800

    Update: latest code and assets for TicketApp

--- lib/view/food/widgets/food_tile.dart
+++ lib/view/food/widgets/food_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:restaurant_foodly/common/app_style.dart';
+import 'package:restaurant_foodly/common/reusable_text.dart';
 import 'package:restaurant_foodly/constants/constants.dart';
 import 'package:restaurant_foodly/constants/uidata.dart';
 
@@ -21,18 +23,64 @@ class FoodTitle extends StatelessWidget {
         color: kOffWhite,
         borderRadius: BorderRadius.circular(12.r)
        ),
-       child: Row(
-        children: [
-          SizedBox(
-            width: 62.w,
-            height: 62.h,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(12.r),
-              child: Image.network(foods['imageUrl'], fit: BoxFit.cover),
-            ),
-          )
-        ],
+       child: Stack(
+         children: [
+           Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 8.w),
+                child: SizedBox(
+                  width: 62.w,
+                  height: 62.h,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12.r),
+                    child: Image.network(foods['imageUrl'], fit: BoxFit.cover),
+                  ),
+                ),
+              ),
+           
+              SizedBox(width: 12.w,),
+           
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ReusableText(text: foods['title'], style: appStyle(11, kDark, FontWeight.w500)),
+                  ReusableText(text: "Delivery Time: ${foods['time']}", style: appStyle(9, kGray, FontWeight.w500)),
+           
+                  SizedBox(
+                    height: 16.h,
+                    width: width*0.7,
+                    child: ListView.builder(
+                      itemCount: foods['additives'].length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context,i){
+                        String text = foods['additives'][i]['title'];
+                        return Container(
+                          margin: EdgeInsets.only(right: 5.w),
+                          decoration: BoxDecoration(
+                            color: kSecondary,
+                            borderRadius: BorderRadius.circular(9.r),
+                          ),
+                          child: Center(
+                            child: Padding(
+                              padding:  EdgeInsets.symmetric(horizontal: 4.w),
+                              child: ReusableText(text: text, style: appStyle(8, kGray, FontWeight.w400)),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  )
+           
+                ],
+              )
+            ],
+           ),
+         ],
        ),
+
+       
       ),
     );
   }
